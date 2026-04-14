6fbb9d95dea1b1f2d9fcce18296c917dc92f195e
Jéssica Pasqualini <jessicadaspasqualini@gmail.com>
2026-04-13 23:12:50 -0300

    adiciona exercício 1014 - Consumo

---
+++ src/beecrowd1014.java
@@ -0,0 +1,36 @@
+/**
+ * Enunciado:
+ *
+ * Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km)
+ * e o total de combustível gasto (em litros).
+ *
+ * Entrada
+ * O arquivo de entrada contém dois valores: um valor inteiro X representando a distância total percorrida (em Km),
+ * e um valor real Y representando o total de combustível gasto, com um dígito após o ponto decimal.
+ *
+ * Saída
+ * Apresente o valor que representa o consumo médio do automóvel com 3 casas após a vírgula, seguido da mensagem "km/l".
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class beecrowd1014 {
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        int X;
+        double Y, consumoMedio;
+
+        X = input.nextInt();
+        Y = input.nextDouble();
+
+        consumoMedio = X / Y;
+
+        System.out.printf("%.3f km/l%n", consumoMedio);
+
+        input.close();
+    }
+}
