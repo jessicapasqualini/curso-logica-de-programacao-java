faf821fd0f5e42467434a171e7dd866ecae12b3c
Jéssica Pasqualini <jessicadaspasqualini@gmail.com>
2026-04-13 23:10:36 -0300

    adiciona exercício 1008 - Salário

---
+++ src/beecrowd1008.java
@@ -0,0 +1,38 @@
+/**
+ * Enunciado:
+ *
+ * Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas,
+ * o valor que recebe por hora e calcula o salário desse funcionário.
+ * A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+ *
+ * Entrada
+ * O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais, representando o número,
+ * quantidade de horas trabalhadas e o valor que o funcionário recebe por hora trabalhada, respectivamente.
+ *
+ * Saída
+ * Imprima o número e o salário do funcionário, conforme exemplo fornecido, com um espaço em branco antes e depois
+ * da igualdade. No caso do salário, também deve haver um espaço em branco após o $.
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class beecrowd1008 {
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        int numeroFuncionario, quantidadeHorasTrabalhadas;
+        double valorPorHoraTrabalhada, salario;
+
+        numeroFuncionario = input.nextInt();
+        quantidadeHorasTrabalhadas = input.nextInt();
+        valorPorHoraTrabalhada = input.nextDouble();
+
+        salario = quantidadeHorasTrabalhadas * valorPorHoraTrabalhada;
+
+        System.out.println("NUMBER = " + numeroFuncionario);
+        System.out.printf("SALARY = U$ %.2f\n", salario);
+    }
+}
