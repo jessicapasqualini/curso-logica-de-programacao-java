9afb2abaa0bf0494ec13b3785b95110568e6386e
Jéssica Pasqualini <jessicadaspasqualini@gmail.com>
2026-04-13 23:09:28 -0300

    adiciona exercício 1007 - Diferença

---
+++ src/beecrowd1007.java
@@ -0,0 +1,36 @@
+/**
+ * Enunciado:
+ *
+ * Leia quatro valores inteiros A, B, C e D.
+ * A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D
+ * segundo a fórmula: DIFERENCA = (A * B - C * D).
+ *
+ * Entrada
+ * O arquivo de entrada contém 4 valores inteiros.
+ *
+ * Saída
+ * Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo,
+ * com um espaço em branco antes e depois da igualdade.
+ */
+
+import java.util.Scanner;
+
+public class beecrowd1007 {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        int A, B, C, D, DIFERENCA;
+
+        A = input.nextInt();
+        B = input.nextInt();
+        C = input.nextInt();
+        D = input.nextInt();
+
+        DIFERENCA = (A * B - C * D);
+
+        System.out.println("DIFERENCA = " + DIFERENCA);
+
+        input.close();
+    }
+}
