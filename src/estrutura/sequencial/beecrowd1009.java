6b96afc915851b3bb11cf2b62d120b2212d109f5
Jéssica Pasqualini <jessicadaspasqualini@gmail.com>
2026-04-15 19:29:15 -0300

    feat: adiciona exercício 1009 - Salário com Bônus

---
+++ src/estrutura/sequencial/beecrowd1009.java
@@ -0,0 +1,42 @@
+package estrutura.sequencial;
+
+/**
+ * Enunciado:
+ *
+ * Faça um programa que leia o nome de um vendedor, o seu salário fixo e
+ * o total de vendas efetuadas por ele no mês (em dinheiro).
+ * Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas,
+ * informar o total a receber no final do mês, com duas casas decimais.
+ *
+ * Entrada
+ * O arquivo de entrada contém um texto (primeiro nome do vendedor) e
+ * 2 valores de dupla precisão (double) com duas casas decimais, representando
+ * o salário fixo do vendedor e montante total das vendas efetuadas por este vendedor, respectivamente.
+ *
+ * Saída
+ * Imprima o total que o funcionário deverá receber, conforme exemplo fornecido - TOTAL = R$ .
+ */
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class beecrowd1009 {
+    public static void main(String[] args){
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        String nomeVendedor;
+        double salarioFixo, vendasEfetuadas, totalReceber;
+
+        nomeVendedor = input.nextLine();
+        salarioFixo = input.nextDouble();
+        vendasEfetuadas = input.nextDouble();
+
+        totalReceber = salarioFixo + (vendasEfetuadas * 0.15);
+
+        System.out.printf("TOTAL = R$ %.2f%n", totalReceber);
+
+        input.close();
+    }
+}
