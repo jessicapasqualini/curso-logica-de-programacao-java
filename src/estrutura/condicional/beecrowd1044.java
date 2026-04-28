379610fa7cb9381fbdbf2a79aa9a416ceab2c8bf
Jéssica Pasqualini <jessicadaspasqualini@gmail.com>
2026-04-28 19:45:04 -0300

    feat: adiciona exercício 1044 - Múltiplos

---
+++ src/estrutura/condicional/beecrowd1044.java
@@ -0,0 +1,36 @@
+package estrutura.condicional;
+
+/**
+ * Enunciado:
+ *
+ * Leia 2 valores inteiros (A e B). Após, o programa deve mostrar uma mensagem "Sao Multiplos" ou "Nao sao Multiplos",
+ * indicando se os valores lidos são múltiplos entre si.
+ *
+ * Entrada
+ * A entrada contém valores inteiros.
+ *
+ * Saída
+ * A saída deve conter uma das mensagens conforme descrito acima.
+ **/
+
+import java.util.Scanner;
+
+public class beecrowd1044 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int A, B;
+
+        A = input.nextInt();
+        B = input.nextInt();
+
+        if (A % B == 0 || B % A == 0) {
+            System.out.println("Sao Multiplos");
+        }
+        else {
+            System.out.println("Nao sao Multiplos");
+        }
+        input.close();
+    }
+}
